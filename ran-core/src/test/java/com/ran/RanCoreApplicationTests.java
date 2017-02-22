d0554fe92fa52ca1d63804f7a101284cb5651c46
zhangran <zhangran42@126.com>
2017-02-23 00:16:31 +0800

    first commit

---
+++ ran-core/src/test/java/com/ran/RanCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ran;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RanCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
